bd3ef0c5a6ce8793e30015d18fb11478a0f49f30
Luis Cedano <king.romanus1@gmail.com>
2015-04-16 10:56:52 -0700

    Re-connection Sourcetree

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+//Why is this important? Why does localhost have to be first?
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
+//$mysqli->connect_error then we want it to die and have this message
+if($mysqli->connect_error) {
+  die('Connect Error (' . $mysqli->connect_errno .')'
+    . $msqli->connect_error);
+}
+else {
+  echo "Connection made";
+}
+$mysqli->close();
+
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Luis To-Do List</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+  </head>
+  <body>
+    <div class="wrap">
+      
+    </div>
+  </body>
+</html>
